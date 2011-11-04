b0681aff9ada2689a1709a2e727d21af18af29e1
Oleg Puzanov <puzanov@gmail.com>
2011-11-04 17:18:53 +0600

    it's workiing

--- gootrans.php
+++ gootrans.php
@@ -1,7 +1,7 @@
 <?
-
-$tr = new Gootrans();
-$tr->translate("Impressive Russian Balalaika Playing", "en", "ru");
+//example
+//$tr = new Gootrans();
+//echo $tr->translate("Impressive Russian Balalaika Playing", "en", "ru");
 
 class Gootrans {
   var $user_agent = "Opera/9.80 (Windows NT 6.1; U; ru) Presto/2.8.131 Version/11.10";  
@@ -16,9 +16,9 @@ class Gootrans {
     curl_setopt($ch, CURLOPT_TIMEOUT, 3); // times out after 4s
     curl_exec($ch); // run the whole process
     curl_setopt($ch, CURLOPT_URL, "http://translate.google.com/translate_a/t?client=t&text=$text&hl=ru&sl=$from&tl=$to");
-    $result = curl_exec($ch);
-    var_dump($result);
+    $result = explode(",", str_replace(array("[[[", "\""), "", curl_exec($ch)));
     curl_close($ch);
+    return $result[0];
   }
 }
 
